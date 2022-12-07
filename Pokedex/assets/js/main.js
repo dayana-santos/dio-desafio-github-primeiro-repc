3e2cda3a28e780c3679a21cd6356bdf6394ebd8d
DESKTOP-CUSN0GU\dyana <ux.dayanasantos@gmail.com>
2022-12-06 23:04:43 -0300

    encadeamento de .then para simplificar a leitura de código

--- Pokedex/assets/js/main.js
+++ Pokedex/assets/js/main.js
@@ -6,7 +6,7 @@ Para iniciar o servidor local: http-server ./
 fetch indica que será utilizado processamento assíncrono, onde
  é enviada a requisição e aguardo resposta (promise) 
 
-exemplo:
+exemplo de Promise:
 
 fetch(url).then(function (response) {
     console.log(response)
@@ -22,20 +22,25 @@ console.log(x);
 
 *********por padrão. o fetch é uma requisição do tipo GET ************
 
+=> (lê-se arrow function), é uma sintaxe reduzida, pra simplificar o
+código
+
+
 */
 
 const offset = 0;
 const limit = 10;
 const url = 'https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}';
 
+/*
+encadeamento de .then para simplificar a leitura de código
+sempre o que fica no 2º .then é o retorno do 1º
+*/
+
+
 fetch(url)
-    .then(function (response) {
-        console.log(response)
-    })
-    .catch(function (error) {
-        console.error(error)
-    })
-    .finally(function () {
-        console.log('Requisição concluída!')
-    })
+    .then((response) => response.json())
+    .then((jsonBody) => console.log(jsonBody))
+    .catch((error) => console.error(error))
+
 
